f7f40b4bc0671d3f240de16fe219fce6267d5572
Sam Lowry <lowrysa@email.sc.edu>
2021-07-03 14:11:27 -0400

    another sprint

--- run.java
+++ run.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class run {
     private static backend backend;
     private static Scanner k;
+    private static textoptions text;
     public static void main(String args[]) {
         intro();
         loop();
@@ -14,15 +15,9 @@ public class run {
 
     public static void intro() {
         clear();
-        p("*******************************************************************************************");
-        p("*                                                                                         *");
-        System.out.print("*                         ");
-        System.out.print("Welcome to the budget tracking program!");
-        System.out.print("                         *");
-        p("*                                                                                         *");
-        p("*******************************************************************************************");
-        p("");
         backend = new backend();
+        text = new textoptions();
+        text.greetings();
         k = new Scanner(System.in);
         backend.readFile();
     }
@@ -30,7 +25,7 @@ public class run {
     public static void loop() {
         boolean over = false;
         while (!over) {
-            printLoop();
+            text.printLoop();
             int choice = k.nextInt();
             if (choice == 1) { //Print Entries
                 clear();
@@ -38,13 +33,12 @@ public class run {
                 if (backend.totalSpent() > 0.0) {
                     boolean overa = false;
                     while (!overa) {
-                        firstChoice();
+                        text.firstChoice();
                         int choicea = k.nextInt();
-                        boolean reallyOver = false;
                         if (choicea == 1) {
                             boolean overDate = false;
                             while(!overDate) {
-                                dateChoice();
+                                text.dateChoice();
                                 int choiceDate = k.nextInt();
                                 if (choiceDate == 1) {
                                     backend.sortByDateLH();
@@ -64,7 +58,7 @@ public class run {
                         } else if (choicea == 2) {
                             boolean overAmount = false;
                             while(!overAmount) {
-                                amountChoice();
+                                text.amountChoice();
                                 int choiceAmount = k.nextInt();
                                 if (choiceAmount == 1) {
                                     backend.sortAmountLH();
@@ -145,12 +139,12 @@ public class run {
                 boolean back1 = false;
                 String entryType = "";
                 while(!overc) {
-                    whatTypeOfEntry();
+                    text.whatTypeOfEntry();
                     var choice1 = k.nextInt();
                     if (choice1 == 1) {
                         boolean over2 = false;
                         while (!over2) {
-                            foodChoice();
+                            text.foodChoice();
                             int choic = k.nextInt();
                             if (choic == 1) {
                                 entryType = "Eat Out";
@@ -199,7 +193,7 @@ public class run {
                     p("What day did you purchase it?");
                     System.out.print("Month: ");
                     //p("What month?");
-                    monthChoice();
+                    text.monthChoice();
                     int monthChoice;
                     int dayChoice;
                     int yearChoice;
@@ -538,89 +532,7 @@ public class run {
             } else 
                 p("Not valid input\n");
         }
-    }
-
-    public static void printLoop() {
-        //clear();
-        p("What would you like to do?");
-        p("1. Print entries");
-        p("2. Check stats");
-        p("3. Add entry");
-        p("4. Check/Change dates");
-        p("8. Show random entry");
-        p("9. Quit program");
-    }
-
-    public static void firstChoice() {
-        //clear();
-        p("How would you like to print?");
-        p("1. Print by date");
-        p("2. Print by amount");
-        p("3. Print by category");
-        p("4. Print in order of added");
-        p("9. Back");
-    }
-
-    public static void dateChoice() {
-        clear();
-        p("How would you like to sort?");
-        p("1. Sort by oldest to newest");
-        p("2. Sort by newest to oldest");
-        p("9. Back");
-    }
-
-    public static void amountChoice() {
-        clear();
-        p("How would you like to sort?");
-        p("1. Sort by low to high");
-        p("2. Sort by high to low");
-        p("9. Back");
-    }
-
-    public static void alphabeticChoice() {
-        clear();
-        p("How would you like to sort?");
-        p("1. Sort from A->Z");
-        p("2. Sort from Z->A");
-        p("9. Back");
-    }
-
-    public static void whatTypeOfEntry() {
-        clear();
-        p("What Type of Entry is it?");
-        p("1. Food");
-        p("2. Gas");
-        p("3. Clothes");
-        p("4. Gecko");
-        p("5. Nintendo");
-        p("8. Other");
-        p("9. Back");
-    }
-
-    public static void foodChoice() {
-        clear();
-        p("What kind of food purchase?");
-        p("1. Eat Out");
-        p("2. Groceries");
-        p("3. Ice Cream");
-    }
-
-    public static void monthChoice() {
-        clear();
-        p("Select the correct month:");
-        p("1. January");
-        p("2. February");
-        p("3. March");
-        p("4. April");
-        p("5. May");
-        p("6. June");
-        p("7. July");
-        p("8. August");
-        p("9. September");
-        p("10. October");
-        p("11. November");
-        p("12. December");
-    }
+    } 
 
     public static void end() {
         p("Saving...");
---
+++ textoptions.java
@@ -0,0 +1,103 @@
+public class textoptions {
+    public textoptions() {}
+
+    public void greetings() {
+        p("*******************************************************************************************");
+        p("*                                                                                         *");
+        System.out.print("*                         ");
+        System.out.print("Welcome to the budget tracking program!");
+        System.out.print("                         *");
+        p("*                                                                                         *");
+        p("*******************************************************************************************");
+        p("");
+    }
+
+    public void foodChoice() {
+        clear();
+        p("What kind of food purchase?");
+        p("1. Eat Out");
+        p("2. Groceries");
+        p("3. Ice Cream");
+    }
+
+    public void printLoop() {
+        p("What would you like to do?");
+        p("1. Print entries");
+        p("2. Check stats");
+        p("3. Add entry");
+        p("4. Check/Change dates");
+        p("8. Show random entry");
+        p("9. Quit program");
+    }
+
+    public void firstChoice() {
+        p("How would you like to print?");
+        p("1. Print by date");
+        p("2. Print by amount");
+        p("3. Print by category");
+        p("4. Print in order of added");
+        p("9. Back");
+    }
+
+    public void dateChoice() {
+        clear();
+        p("How would you like to sort?");
+        p("1. Sort by oldest to newest");
+        p("2. Sort by newest to oldest");
+        p("9. Back");
+    }
+
+    public void amountChoice() {
+        clear();
+        p("How would you like to sort?");
+        p("1. Sort by low to high");
+        p("2. Sort by high to low");
+        p("9. Back");
+    }
+
+    public void alphabeticChoice() {
+        clear();
+        p("How would you like to sort?");
+        p("1. Sort from A->Z");
+        p("2. Sort from Z->A");
+        p("9. Back");
+    }
+
+    public void whatTypeOfEntry() {
+        clear();
+        p("What Type of Entry is it?");
+        p("1. Food");
+        p("2. Gas");
+        p("3. Clothes");
+        p("4. Gecko");
+        p("5. Nintendo");
+        p("8. Other");
+        p("9. Back");
+    }
+
+    public void monthChoice() {
+        clear();
+        p("Select the correct month:");
+        p("1. January");
+        p("2. February");
+        p("3. March");
+        p("4. April");
+        p("5. May");
+        p("6. June");
+        p("7. July");
+        p("8. August");
+        p("9. September");
+        p("10. October");
+        p("11. November");
+        p("12. December");
+    }
+
+    public static void clear() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void p(String s) {
+        System.out.println(s);
+    }
+}
